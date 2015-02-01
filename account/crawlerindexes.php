c1323c4f05d55bb88a22ca3d1a54e6fa442dbc13
kchizi <kchiziwazzy@gmail.com>
2015-02-01 21:28:50 +0300

    indexing active
    incorporated editer for indexes

---
+++ account/crawlerindexes.php
@@ -0,0 +1,112 @@
+<?php
+
+require_once("../models/config.php");
+
+// Request method: GET
+$ajax = checkRequestMode("get");
+
+if (!securePage(__FILE__)){
+    apiReturnError($ajax);
+}
+
+setReferralPage(getAbsoluteDocumentPath(__FILE__));
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <?php
+  	echo renderAccountPageHeader(array("#SITE_ROOT#" => SITE_ROOT, "#SITE_TITLE#" => SITE_TITLE, "#PAGE_TITLE#" => "Dashboard"));
+  ?>
+
+  <body>
+
+    <div id="wrapper">
+
+      <!-- Sidebar -->
+        <?php
+          echo renderMenu("dashboard");
+        ?>  
+
+      <div id="page-wrapper">
+	  	<div class="row">
+          <div id='display-alerts' class="col-lg-12">
+          
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            <h1>Dashboard <small>Crawler Indexes</small></h1>
+            <ol class="breadcrumb">
+              <li class="active"><i class="fa fa-search"></i> Dashboard</li>
+            </ol>
+            <div class="alert alert-success alert-dismissable">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+              Welcome to Unisearch!  We help you plan your future. 
+            </div>
+          </div>
+        </div><!-- /.row -->
+
+
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-clock-o"></i> Recent Activity</h3>
+              </div>
+              <div class="panel-body">
+                <div class="list-group">
+                  <a href="#" class="list-group-item">
+                    <span class="badge">just now</span>
+                    <i class="fa fa-calendar"></i> Calendar updated
+                  </a>
+                  <a href="#" class="list-group-item">
+                    <span class="badge">4 minutes ago</span>
+                    <i class="fa fa-comment"></i> Commented on a post
+                  </a>
+                  <a href="#" class="list-group-item">
+                    <span class="badge">23 minutes ago</span>
+                    <i class="fa fa-truck"></i> Order 392 shipped
+                  </a>
+                  <a href="#" class="list-group-item">
+                    <span class="badge">46 minutes ago</span>
+                    <i class="fa fa-money"></i> Invoice 653 has been paid
+                  </a>
+                  <a href="#" class="list-group-item">
+                    <span class="badge">1 hour ago</span>
+                    <i class="fa fa-user"></i> A new user has been added
+                  </a>
+                  <a href="#" class="list-group-item">
+                    <span class="badge">2 hours ago</span>
+                    <i class="fa fa-check"></i> Completed task: "pick up dry cleaning"
+                  </a>
+                  <a href="#" class="list-group-item">
+                    <span class="badge">yesterday</span>
+                    <i class="fa fa-globe"></i> Saved the world
+                  </a>
+                  <a href="#" class="list-group-item">
+                    <span class="badge">two days ago</span>
+                    <i class="fa fa-check"></i> Completed task: "fix error on sales page"
+                  </a>
+                </div>
+                <div class="text-right">
+                  <a href="#">View All Activity <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div><!-- /.row -->
+
+      </div><!-- /#page-wrapper -->
+
+    </div><!-- /#wrapper -->
+
+	<script>
+        $(document).ready(function() {       
+          alertWidget('display-alerts');
+		});
+	</script>
+  </body>
+</html>
+
+
